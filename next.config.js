b5c308d00d233087380644239112082564ccd239
unknown <jdotfite@gmail.com>
2025-04-29 09:43:10 -0400

    next js change to build for vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      }
+    ];
+  },
+  // This ensures proper Vercel deployment
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
