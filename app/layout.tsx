e2fb475c505fce48d53cc049481556544e70f34f
Justin Fite <jdotfite@gmail.com>
2025-04-30 19:10:00 -0400

    import case error in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
-import IOSFullscreen from '@/components/IOSFullscreen';
+import IOSFullscreen from '@/components/IOSFullScreen';
 import { ThemeProvider } from '@/lib/context/ThemeContext';
 import AppContainer from '@/components/layout/AppContainer';
 import Script from 'next/script';
